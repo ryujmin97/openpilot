7a1555edf4dcf837b5593cdf762f0eb6e7e6d883
ryujmin97 <__@example.com>
2026-09-17 16:37:35 +0900

    74cha: params_keys.h CarrotGDriveClientId/Secret/RefreshToken 등록 재적용 (22cha 48c2e081, 항목9)

--- openpilot/common/params_keys.h
+++ openpilot/common/params_keys.h
@@ -380,6 +380,9 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"CarrotException", {CLEAR_ON_MANAGER_START, STRING}},
     {"CarrotExceptionSent", {CLEAR_ON_MANAGER_START, BOOL}},
     {"CarrotExceptionDiscordWebhookUrl", {PERSISTENT, STRING}},
+    {"CarrotGDriveClientId", {PERSISTENT, STRING}},
+    {"CarrotGDriveClientSecret", {PERSISTENT, STRING}},
+    {"CarrotGDriveRefreshToken", {PERSISTENT, STRING}},
     {"CwebPushRecoveryBoot", {PERSISTENT, BOOL, "0"}},
 
     {"CarName", {PERSISTENT, STRING}},
